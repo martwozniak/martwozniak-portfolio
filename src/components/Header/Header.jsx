89b215be2408ca6459db05a37f3f43b792465799
Martin <persky03@gmail.com>
2022-05-20 18:00:08 +0200

    Jest configuration

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+
+function Header() {
+  return (
+      <div className="flex justify-center">
+          <div className='container text-sky-400'>
+                <span className="font-semibold">Marcin Woźniak</span>
+          </div>
+    </div>
+  )
+}
+
+export default Header

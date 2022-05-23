d13f0f045a47bc448b03c1108601b6f5ce019999
Martin <persky03@gmail.com>
2022-05-23 23:06:59 +0200

    Storybook

---
+++ src/stories/Footer.stories.js
@@ -0,0 +1,8 @@
+import Footer from "../components/Footer/Footer";
+
+export default {
+  title: "Footer",
+  component: Footer,
+};
+
+export const Default = () => <Footer />;
---
+++ src/stories/Header.stories.js
@@ -0,0 +1,14 @@
+import Header from "../components/Header/Header";
+
+export default {
+  title: "Header",
+  component: Header,
+};
+
+// const Template = (args) => <Header {...args} />;
+export const Default = () => <Header />;
+// export const Default = () => Template.bind({});
+
+// Default.args = {
+
+// }
